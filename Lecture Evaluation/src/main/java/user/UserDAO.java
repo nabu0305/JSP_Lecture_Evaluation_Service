35b243eb40081999fb2cc2e30014c97beabc97d3
leeminjun <sohwi0305@gmail.com>
2023-08-16 13:10:39 +0900

    회원 데이터 모델링

--- Lecture Evaluation/src/main/java/user/UserDAO.java
+++ Lecture Evaluation/src/main/java/user/UserDAO.java
@@ -59,4 +59,70 @@ public class UserDAO {
 		}
 		return -1; //회원가입 실패
 	}
+	
+	public String getUserEmail(String userID) {
+		String SQL = "SELECT userEmail FROM USER WHERE userID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs=null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getString(1);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null)conn.close(); } catch(Exception e) {e.printStackTrace();}
+			try {if(pstmt != null)pstmt.close(); } catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null)rs.close(); } catch(Exception e) {e.printStackTrace();}
+		}
+		return null; //데이터베이스 오류
+	}
+	
+	public boolean getUserEmailChecked(String userID) {
+		String SQL = "SELECT userEmailChecked FROM USER WHERE userID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs=null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getBoolean(1);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null)conn.close(); } catch(Exception e) {e.printStackTrace();}
+			try {if(pstmt != null)pstmt.close(); } catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null)rs.close(); } catch(Exception e) {e.printStackTrace();}
+		}
+		return false; //데이터베이스 오류
+	}
+	public boolean setUserEmailChecked(String userID) {
+		String SQL = "UPDATE USER SET userEmailChecked = true WHERE userID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs=null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			pstmt.executeUpdate();
+			return true;
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			try {if(conn != null)conn.close(); } catch(Exception e) {e.printStackTrace();}
+			try {if(pstmt != null)pstmt.close(); } catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null)rs.close(); } catch(Exception e) {e.printStackTrace();}
+		}
+		return false; //데이터베이스 오류
+	}
 }
